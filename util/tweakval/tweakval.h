64c130a4b1196c7379d8e00779c6fd88cae57639
joeld42 <joeld42@710bf54c-7b4f-11de-acf3-6b2d83488292>
2009-08-28 07:45:13 +0000

    Got tweakval working.

--- util/tweakval/tweakval.h
+++ util/tweakval/tweakval.h
@@ -1,9 +1,55 @@
 #ifndef TWEAKVAL_H
 #define TWEAKVAL_H
+//=========================================================
+// tweakval.h
+//
+// Author: Joel Davis (joeld42@yahoo.com)
+// 
+// Based on a discussion on this thread:
+// https://mollyrocket.com/forums/viewtopic.php?t=556
+// See that thread for details.
+//
+// USAGE: Anywhere you're using a constant value that 
+// you want to be tweakable, wrap it in a _TV() macro. 
+// In a release build (with -DNDEBUG defined) this 
+// will simply compile out to the constant, but in a 
+// debug build this will scan the source file for 
+// a modified value, providing a "live update" in 
+// the game.
+//
+// Simply edit the value in the source file, and save the file
+// while the game is running, and you'll see the change.
+//
+//  EXAMPLES:
+//     // Move at constant (tweakable) speed
+//     float newPos = oldPos + _TV(5.2f) * dt;
+//
+//     // Clear to a solid color
+//     glClearColor( _TV( 0.5 ), _TV( 0.2 ), _TV( 0.9 ), 1.0 );
+//     glClear( GL_COLOR_BUFFER_BIT );
+//
+//     // initial monster health
+//     Monster *m = new Monster( _TV( 10 ) );
+//
+//   WARNING: 
+//   This is currently in a very rough state, it doesn't
+//   handle errors, c-style comments, and adding and removing or
+//   rearranging TV's will probably confuse it.
+//   Also, putting _TV's in header files is not a good idea.
+//=========================================================
+
 
 // Note: gcc supports __COUNTER__ in 4.3+
+//       msvc supports it in 6.0+
+//
+// See the thread referenced above for idea of how to implement
+// this without the __COUNTER__ macro.
 #ifndef NDEBUG
-#  define _TV(Val) TweakValue( __FILE__, __LINE__, __COUNTER__, Constant )
+#  define _TV(Val) _TweakValue( __FILE__, __COUNTER__, Val )
+
+float _TweakValue( const char *file, size_t counter, float origVal );
+int _TweakValue( const char *file, size_t counter, int origVal );
+
 #else
 #  define _TV(Val) Val
 #endif
