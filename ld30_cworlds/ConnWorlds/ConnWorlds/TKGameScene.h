73180659f8c0586b8923640191e11b1fff5ba5b8
joeld42 <joeld42@710bf54c-7b4f-11de-acf3-6b2d83488292>
2014-08-23 19:03:10 +0000

    Set up empty game scene

---
+++ ld30_cworlds/ConnWorlds/ConnWorlds/TKGameScene.h
@@ -0,0 +1,14 @@
+//
+//  TKGameScene.h
+//  ConnWorlds
+//
+//  Created by Joel Davis on 8/23/14.
+//  Copyright (c) 2014 Tapnik. All rights reserved.
+//
+
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface TKGameScene : SKScene
+
+@end
---
+++ ld30_cworlds/ConnWorlds/ConnWorlds/TKGameScene.m
@@ -0,0 +1,13 @@
+//
+//  TKGameScene.m
+//  ConnWorlds
+//
+//  Created by Joel Davis on 8/23/14.
+//  Copyright (c) 2014 Tapnik. All rights reserved.
+//
+
+#import "TKGameScene.h"
+
+@implementation TKGameScene
+
+@end
