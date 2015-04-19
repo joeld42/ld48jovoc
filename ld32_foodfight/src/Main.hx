e2dee3c0439cdb031f28dcd6c8a767e5256f3d98
Joel Davis <joeld42@gmail.com>
2015-04-19 09:22:08 -0700

    Updated level geo, added grinder

--- ld32_foodfight/src/Main.hx
+++ ld32_foodfight/src/Main.hx
@@ -7,6 +7,7 @@ import luxe.ParcelProgress;
 import luxe.Color;
 import luxe.Input;
 import luxe.Text;
+import luxe.utils.Maths;
 
 import luxe.collision.ShapeDrawerLuxe;
 import luxe.collision.Collision;
@@ -35,20 +36,29 @@ class Main extends luxe.Game {
     var inputUp : Vector;
     var inputDown : Vector;
 
+    // Camera
+    var cameraTarget : Vector;
+
+
     // Game World
     var walls : Array<Shape>;
     var playerShape : Shape;
+    var grinderShape : Shape;
     var bullets : Array<Bullet>;
+    var splatterMeshes : Array<Mesh>;
 
     // Geometry
     var playerDir : Vector;
     var strafeDir : Vector;
 	var meshWorld : Mesh;	
 	var meshPlayer : Mesh;
+    var meshGrinder : Mesh;
     var meshBulletSrc : Mesh;
+    var firing : Bool = false;
+    var shootyTimeout : Float;
 
     // Everything is ready
-    var gameReadySemaphore : Int = 2;
+    var gameReadySemaphore : Int = 4;
 
     // Debug and HUD tools
     var hud_batcher:Batcher;
@@ -88,16 +98,26 @@ class Main extends luxe.Game {
 		// Player
         var texPlayer = Luxe.loadTexture('assets/tmp_player.png');    		
     	meshPlayer = new Mesh({ file:'assets/ld32_foodfight_tmp_player.obj', 
-    						texture:texPlayer, onload:meshloaded });
+    						texture:texPlayer, onload: function (m:Mesh) {
+                                    m.pos.set_xyz( 0.0, 0.0, 5.0 );
+                                    playerShape.position.set_xy( m.pos.x, m.pos.z );
+                                    meshloaded(m);
+                                } });
 
         // Bullet
         var texBullet = Luxe.loadTexture('assets/peas.png');          
         meshBulletSrc = new Mesh({ file:'assets/ld32_foodfight_cube.obj', 
                             texture:texBullet, onload: function (m : Mesh ) {
                                 m.geometry.visible = false;
-                                meshloaded(null);
+                                meshloaded(m);
                              }});
-                            
+
+        // Grinder
+        var texGrinder = Luxe.loadTexture('assets/ld32_footfight_grinder.png');          
+        meshGrinder = new Mesh({ file:'assets/ld32_foodfight_grinder.obj', 
+                            texture:texGrinder, onload: function (m : Mesh ) {                                
+                                meshloaded(m);
+                             }});                            
 
         // Create the HUD
         create_hud();
@@ -145,13 +165,19 @@ class Main extends luxe.Game {
         playerDir = new Vector( 0.0, 0.0, -1.0 );
         strafeDir = playerDir.clone();
         bullets = new Array<Bullet>();
+        splatterMeshes = new Array<Mesh>();
+        cameraTarget = new Vector();
 
 		var preload = new Parcel();    	
     	preload.add_texture( "assets/ld32_foodfight_env.png");
     	preload.add_texture( "assets/tmp_player.png");
+        preload.add_texture( "assets/peas.png");
+        preload.add_texture( "assets/ld32_foodfight_grinder.png");
 
 		preload.add_text( "assets/ld32_foodfight_env.obj" );
 		preload.add_text( "assets/ld32_foodfight_tmp_player.obj" );
+        preload.add_text( "assets/ld32_foodfight_cube.obj" );
+        preload.add_text( "assets/ld32_foodfight_grinder.obj" );
 
 		new ParcelProgress({
             parcel      : preload,
@@ -179,13 +205,22 @@ class Main extends luxe.Game {
         Luxe.input.bind_key( 'movedown', Key.key_s );
 
         // Create the game world
+        grinderShape = new Circle( 0.0, 0.0, 2.0 );
         walls = [
             // confusing: it's center, w, h
             Polygon.rectangle( -11.0, 0.0, 2.0, 25.0),
             Polygon.rectangle(  11.0, 0.0, 2.0, 25.0),
             Polygon.rectangle(  0.0, -11.0, 25.0, 2.0),
             Polygon.rectangle(  0.0, 11.0, 25.0, 2.0),
-            new Circle( 5.0, 5.0, 3.5 )
+            
+            grinderShape,
+
+            // tables
+            new Circle( -3.0, 5.5, 1.8 ),
+            new Circle(  3.0, 5.5, 1.8 ),
+            Polygon.rectangle(  0.0, -5.3, 7.2, 1.8),
+            Polygon.rectangle(  6.7, 0.0, 1.8, 7.2 ),
+            Polygon.rectangle(  -6.7, 0.0, 1.8, 7.2 )
         ];
 
         playerShape = new Circle( 0.0, 0.0, 0.4 );
@@ -240,11 +275,15 @@ class Main extends luxe.Game {
         var bullet = new Bullet(cloneMesh( meshBulletSrc ));
         
         bullet.mesh.pos.set_xyz( meshPlayer.pos.x, 1.0, meshPlayer.pos.z );
-        bullet.vel.copy_from( strafeDir );
+        bullet.vel.set_xyz( strafeDir.x + Maths.random_float( -0.1, 0.1 ),
+            strafeDir.y + Maths.random_float( -0.1, 0.1 ),
+            strafeDir.z + Maths.random_float( -0.1, 0.1 ) );
+
         bullets.push( bullet );        
+
+        shootyTimeout = 0.1;
     }
 
-    var next_trace:Float = 0.0;
     override function update(dt:Float) {
 
         // Is everything initted?
@@ -253,6 +292,7 @@ class Main extends luxe.Game {
         }
 
         // Collisions and movement
+        var playerDead = false;
         var oldPlayerPos = meshPlayer.pos.clone();
 
         // update input dir (the 0.1 is for substeps)                       
@@ -283,6 +323,12 @@ class Main extends luxe.Game {
 
                 // trace('COLLIDE: ${coll} ${sep} ${collisions.length}');
 
+                if (coll.shape2 == grinderShape)
+                {
+                    // OOps.. hit the grinder.. u ded
+                    playerDead = true;
+                }
+
                 var mover_separated_pos = new Vector( coll.shape1.position.x + sep.x*1.01, 
                                                       coll.shape1.position.y + sep.y*1.01 );
 
@@ -295,7 +341,17 @@ class Main extends luxe.Game {
             }
         }
 
+        // check collide with enemies
+        // TODO
+
+        // commit new player position
         playerShape.position.set_xy( meshPlayer.pos.x, meshPlayer.pos.z );
+
+        // Check for game over
+        if (playerDead) {
+            // TODO: need better splat effect
+            resetGame( );
+        }
 
         // calc player direction
         var testPlayerDir = new Vector( meshPlayer.pos.x - oldPlayerPos.x,
@@ -307,15 +363,6 @@ class Main extends luxe.Game {
         }
 
         // Shooty shooty
-        var firing = false;
-        if(Luxe.input.inputdown('fire')) {
-            if(Luxe.time > next_trace) {
-                trace('fire is held down');
-                next_trace = Luxe.time + 0.4;
-                firing = true;
-            }
-        }
-
         if(Luxe.input.inputpressed('fire')) {
             trace('fire pressed in update');
             fireUnconventionalWeapon( dt );
@@ -324,10 +371,18 @@ class Main extends luxe.Game {
 
         if(Luxe.input.inputreleased('fire')) {
             trace('fire released in update');
+            firing = false;
         }
 
         if (!firing) {
+            desc.text = 'Not Shooting';
             strafeDir.copy_from( playerDir );
+        } else {
+            desc.text = 'Shooting: ${shootyTimeout}';
+            shootyTimeout -= dt;            
+            if (shootyTimeout <= 0.0) {
+                fireUnconventionalWeapon(dt);
+            }
         }
 
         // Update bullets
@@ -345,7 +400,17 @@ class Main extends luxe.Game {
                 bulletShape.position.set_xy( b.mesh.pos.x, b.mesh.pos.z );
                 var collisions = Collision.shapeWithShapes( bulletShape, walls);
                 if (collisions.length>0) {
+
+                    var coll = collisions[0];
+                    var sep = coll.separation;
+                    
+                    b.mesh.pos.set_xyz(coll.shape1.position.x + sep.x*0.5, b.mesh.pos.y,
+                                              coll.shape1.position.y + sep.y*0.5 );
                     expiredBullets.push( b );
+
+                    if (coll.shape2 == grinderShape) {
+                        b.mesh.destroy();
+                    }
                 }
             } 
         }
@@ -353,11 +418,20 @@ class Main extends luxe.Game {
         for ( b in expiredBullets) {
             bullets.remove( b );
 
-            b.mesh.destroy();
-
+            // heheh.. looks cool to leave these
+            splatterMeshes.push( b.mesh );            
         }
 
-        desc.text = 'BULLETS: ${bullets.length}';
+        // Update camera pos        
+        cameraTarget.set_xyz( meshPlayer.pos.x + (strafeDir.x * 3.0), 
+                              meshPlayer.pos.y + 18.0,
+                              meshPlayer.pos.z + (strafeDir.z * 3.0) + 7.5 );
+
+        Luxe.camera.pos.set_xyz( Luxe.camera.pos.x + (cameraTarget.x-Luxe.camera.pos.x) * dt,
+            Luxe.camera.pos.y + (cameraTarget.y-Luxe.camera.pos.y) * dt,
+            Luxe.camera.pos.z + (cameraTarget.z-Luxe.camera.pos.z) * dt );
+
+        
     } //update
 
     override function onrender() {
@@ -366,12 +440,28 @@ class Main extends luxe.Game {
         if (gameReadySemaphore > 0) {
             return;
         }
-
+/*
         for (shape in walls) {
             drawer.drawShape(shape);
         }
 
         drawer.drawShape( playerShape );
+        */
+    }
+
+    function resetGame() {
+        while (bullets.length > 0) {
+            var b = bullets.pop();
+            b.mesh.destroy();
+        }
+
+        while (splatterMeshes.length > 0) {
+            var m = splatterMeshes.pop();
+            m.destroy();
+        }
+
+        meshPlayer.pos.set_xyz( 0.0, 0.0, 5.0 );
+        playerShape.position.set_xy( meshPlayer.pos.x, meshPlayer.pos.z );
     }
 
     // ===============================================
