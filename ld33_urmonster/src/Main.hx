97fb7067ce89683979698fc93ec749a1c5312ab9
Joel Davis <joeld42@gmail.com>
2015-08-22 00:01:56 -0700

    [ld33_urmonster] basic camera control and player movement

--- ld33_urmonster/src/Main.hx
+++ ld33_urmonster/src/Main.hx
@@ -22,7 +22,7 @@ import snow.api.buffers.Uint8Array;
 import snow.system.assets.Assets;
 
 import SceneRender;
-
+import ZillaMover;
 
 class Main extends luxe.Game {
 
@@ -30,10 +30,16 @@ class Main extends luxe.Game {
 	var hudBatcher_ :Batcher;
 
 	var flyCamera_ : FlyCamera;
+	var gameCamera_ : Camera;
+	var gameCameraTarget_ : Vector;
 
 	var scene_ : SceneRender;
 
-	var zilla_ : SceneObj;
+	var zillaObj_ : SceneObj;
+	var zilla_ : Entity;
+	var zillaMover_ : ZillaMover;
+
+	var lookatObj_ : SceneObj;
 
 	override function config( config : luxe.AppConfig )
 	{
@@ -51,7 +57,7 @@ class Main extends luxe.Game {
 	function preloadResources( config : luxe.AppConfig )
 	{
 		// Configure texture
-		var texNames = [ "ground1.jpg", "house.png", "suzilla.png" ];
+		var texNames = [ "ground1.jpg", "house.png", "suzilla.png", "axisGadget.png" ];
 		for (texName in texNames)
 		{
 			config.preload.textures.push({ id :  "assets/" + texName,
@@ -67,6 +73,7 @@ class Main extends luxe.Game {
 		config.preload.bytes.push({ id : "assets/mesh/MESH_Cube.001.dat" });
 		config.preload.bytes.push({ id : "assets/mesh/MESH_GroundMesh.dat" });
 		config.preload.bytes.push({ id : "assets/mesh/MESH_SuzillaMesh.dat" });
+		config.preload.bytes.push({ id : "assets/mesh/MESH_axisGadgetMesh.dat" });
 
 		config.preload.jsons.push( { id : "assets/testland.json" });
 
@@ -83,12 +90,24 @@ class Main extends luxe.Game {
     		cull_backfaces : true,
     		aspect:Luxe.screen.w/Luxe.screen.h
 		});
-    	
-    	scene_.sceneCamera_ = flyCamera_;
-    	Luxe.renderer.batcher.view = flyCamera_.view;
 
-    	flyCamera_.pos.set_xyz(0,20,15);
-    	flyCamera_.rotation.setFromEuler( new Vector( -50.0, 0, 0).radians() );
+		gameCamera_ = new Camera({
+    		name:'flycam',
+    		projection: ProjectionType.perspective,
+    		fov:90,
+    		near:0.1,
+    		far:1000,
+    		cull_backfaces : true,
+    		aspect:Luxe.screen.w/Luxe.screen.h
+		});
+    	
+    	scene_.sceneCamera_ = gameCamera_;
+    	Luxe.renderer.batcher.view = gameCamera_.view;
+
+    	gameCamera_.pos.set_xyz(0,20,15);
+    	gameCamera_.rotation.setFromEuler( new Vector( -50.0, 0, 0).radians() );
+    	gameCameraTarget_ = new Vector();
+    	gameCamera_.view.target = gameCameraTarget_;
 	}
 
     override function ready() 
@@ -114,17 +133,83 @@ class Main extends luxe.Game {
     	scene_.loadScene( "assets/testland.json");
     	scene_.initShadows();
 
-    	zilla_ = scene_.findSceneObj("Suzilla");
+    	zillaObj_ = scene_.findSceneObj("Suzilla");
     	var camPos = new Vector();
-    	camPos.copy_from(zilla_.xform_.pos);
+    	camPos.copy_from(zillaObj_.xform_.pos);
     	
     	flyCamera_.pos.set_xyz( camPos.x, camPos.y + 5.0, camPos.z );
 
+    	lookatObj_ = scene_.findSceneObj( "axisGadget" );
+
+    	bindInput();
+    	setupGame();
     } //ready
+
+ 	function bindInput() {
+
+        Luxe.input.bind_key('left', Key.key_a);
+        Luxe.input.bind_key('left', Key.left);
+
+        Luxe.input.bind_key('right', Key.key_d);
+        Luxe.input.bind_key('right', Key.right);
+
+        Luxe.input.bind_key('up', Key.key_w);
+        Luxe.input.bind_key('up', Key.up);
+
+        Luxe.input.bind_key('down', Key.key_s);
+        Luxe.input.bind_key('down', Key.down);
+
+        // Luxe.input.bind_key('jump', Key.key_w);
+        // Luxe.input.bind_key('jump', Key.up);
+        // Luxe.input.bind_key('jump', Key.space);
+        // Luxe.input.bind_key('jump', Key.key_z);
+        // Luxe.input.bind_key('jump', Key.key_x);
+
+        Luxe.input.bind_key('camera', Key.tab );
+    } //bind_input
+
+	override function oninputup( _input:String, e:InputEvent ) {
+        trace( 'named input up : ' + _input );
+        if (_input=='up') {
+        	zillaMover_.forceUp_ = 0.0;
+        } else if (_input=='down') {
+    		zillaMover_.forceDown_ = 0.0;        
+        } else if (_input=='left') {
+    		zillaMover_.forceLeft_ = 0.0;
+        } else if (_input=='right') {
+    		zillaMover_.forceRight_ = 0.0;
+        }
+    } //oninputup
+
+    override function oninputdown( _input:String, e:InputEvent ) {
+        trace( 'named input down : ' + _input );
+        if (_input=='up') {
+        	zillaMover_.forceUp_ = 1.0;
+        } else if (_input=='down') {
+    		zillaMover_.forceDown_ = 1.0;        
+        } else if (_input=='left') {
+    		zillaMover_.forceLeft_ = 1.0;
+        } else if (_input=='right') {
+    		zillaMover_.forceRight_ = 1.0;
+        }
+    } //oninputdown
+
+    function setupGame()
+    {
+    	// Make our zilla and link it to the sceneObj
+    	zilla_ = new Entity({ name:'zilla' });
+    	zilla_.transform = zillaObj_.xform_;
+
+    	zillaMover_ = new ZillaMover({ name:'zillaMover' });
+ 		zilla_.add( zillaMover_ );
+
+    }
 
     override function onkeyup( e:KeyEvent ) {
 
-        if(e.keycode == Key.escape) {
+    	// Don't use escape because flycamera needs it to
+    	// to grab mouse
+        if(e.keycode == Key.key_k) {
             Luxe.shutdown();
         }
 
@@ -139,7 +224,17 @@ class Main extends luxe.Game {
 
     } //onpostrender
 
-    override function update(dt:Float) {
+    override function update(dt:Float) 
+    {
+    	// Update camera 
+    	var cameraTarg = Vector.Add( zilla_.pos, new Vector( 0.0, 0.0, 10.0 ) );
+    	gameCamera_.pos.set_xyz( zilla_.pos.x, 
+    							 zilla_.pos.y + 30.0, 
+    							 zilla_.pos.z - 20.0 );
+    	gameCameraTarget_.copy_from( cameraTarg );
+
+    	lookatObj_.xform_.pos.copy_from( gameCameraTarget_ );
+
 
     } //update
 
---
+++ ld33_urmonster/src/ZillaMover.hx
@@ -0,0 +1,44 @@
+
+import luxe.Vector;
+
+import luxe.Component;
+import luxe.Entity;
+import phoenix.Quaternion;
+
+class ZillaMover extends Component 
+{
+	// public var rotate_speed : Float = 10;
+	// public var max_rotate_speed : Float = 60;    
+
+	public var forceUp_ : Float = 0.0;
+	public var forceDown_ : Float = 0.0;
+	public var forceLeft_ : Float = 0.0;
+	public var forceRight_ : Float = 0.0;
+
+    override function update( dt:Float ) {
+
+    	trace('up ${forceUp_} down ${forceDown_} left ${forceLeft_} right ${forceRight_}');
+    	
+   
+        var fwd = entity.transform.world.matrix.forward();
+        fwd.normalize();
+        fwd.multiplyScalar( forceUp_ - forceDown_ );
+
+        // var right = entity.transform.world.matrix.right();
+        // right.normalize();
+        // right.multiplyScalar( forceRight_ - forceLeft_ );
+
+        var moveDir = fwd.clone();
+        moveDir.normalize();
+
+        moveDir.multiplyScalar( dt * 20.0 );
+        entity.pos.add( moveDir ); 
+
+        var q = new Quaternion();
+     	q.setFromAxisAngle( new Vector( 0.0, 1.0, 0.0), (forceLeft_-forceRight_)*4.0*dt );
+        entity.rotation.multiply(q);
+
+
+    } //update
+
+} //Rotate
