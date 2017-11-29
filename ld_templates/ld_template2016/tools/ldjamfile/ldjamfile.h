c9908e80c873dd8aaa985430d4118688620fcb43
Joel Davis <joeld42@gmail.com>
2017-11-29 00:24:03 -0800

    Copied in and mangled chunktool for use as ldjamtool

---
+++ ld_templates/ld_template2016/tools/ldjamfile/ldjamfile.h
@@ -0,0 +1,54 @@
+#ifndef LDJAMFILE_H
+#define LDJAMFILE_H
+
+// LDJamFile is exported by ldjamtool (from OGEX) and ingested
+// by runtime. This is the only part that is shared between ldjamtool and
+// runtime.
+#include <stdint.h>
+
+#include "glm/glm.hpp"
+
+
+#pragma pack(push, 1)
+
+struct LDJamFileHeader 
+{
+	uint32_t m_fourCC;    // Expects 'LD48'
+	uint32_t m_numChunks;
+};
+
+
+// Info for each chunk. Infos for all chunks are always loaded with the
+// db header.
+struct LDJamFileMeshInfo
+{
+	char m_name[32];	
+	glm::vec4 m_tintColor;
+
+	glm::vec3 m_bboxMin;
+	glm::vec3 m_bboxMax;
+
+	uint32_t m_contentOffset;
+	uint32_t m_contentLength;
+};
+
+struct LDJamFileMeshContent
+{	
+	uint32_t m_numVerts;
+	uint32_t m_numTris;
+	uint16_t m_triIndices;
+};
+
+struct LDJamFileVertex
+{
+	glm::vec3 m_pos;
+	glm::vec3 m_nrm;
+	glm::vec2 m_st0;  // Base texture ST
+	glm::vec2 m_st1;  // Lightmap ST
+};
+
+#pragma pack(pop)
+
+void LDJamFileVertexTest();
+
+#endif
